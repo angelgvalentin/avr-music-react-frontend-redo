e18477352fa33829e771fa4fa595654f833b023a
Angel Valentin <angelgvalentin7@gmail.com>
2022-07-06 14:49:47 -0500

    rearranged files

--- src/components/Guitars.js
+++ src/components/Guitars.js
@@ -26,37 +26,47 @@ const Guitars = (props) => {
     return (
         <>
             <h1 className="carousel-banner">Placeholder</h1>
-            {allGuitars.length === undefined || allGuitars.length === 0 ? (
-                <h1>Loading...</h1>
-            ) : (
-                <>
-                    <div>Guitars Page</div>
-                    <div className="grid">
-                        {allGuitars.map((guitar) => {
-                            const {id, name, image, price} = guitar;
-                            return (
-                                <Link to={"/details/" + id} key={id}>
-                                    {" "}
-                                    <div>
-                                        <Card style={{width: "18rem"}}>
-                                            <div className="cardImageBox">
-                                                <img className="cardImage" src={image} />
-                                            </div>
 
-                                            <Card.Body>
-                                                <Card.Title>{name}</Card.Title>
-                                                <div className="cardPrice">
-                                                    <Card.Text>${price}</Card.Text>
+            <div>
+                {allGuitars.length === undefined || allGuitars.length === 0 ? (
+                    <h1>Loading...</h1>
+                ) : (
+                    <>
+                        <div>Guitars Page</div>
+                        <div className="grid">
+                            {allGuitars.map((guitar) => {
+                                const {id, name, image, price} = guitar;
+                                return (
+                                    <Link to={"/details/" + id} key={id}>
+                                        {" "}
+                                        <div>
+                                            <Card style={{width: "18rem"}}>
+                                                <div className="cardImageBox">
+                                                    <img
+                                                        className="cardImage"
+                                                        src={image}
+                                                    />
                                                 </div>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                </Link>
-                            );
-                        })}
-                    </div>
-                </>
-            )}
+
+                                                <Card.Body>
+                                                    <Card.Title>
+                                                        {name}
+                                                    </Card.Title>
+                                                    <div className="cardPrice">
+                                                        <Card.Text>
+                                                            ${price}
+                                                        </Card.Text>
+                                                    </div>
+                                                </Card.Body>
+                                            </Card>
+                                        </div>
+                                    </Link>
+                                );
+                            })}
+                        </div>
+                    </>
+                )}
+            </div>
         </>
     );
 };
