d95ea383920dd955d9d37685dfd15d4a24227341
Angel Valentin <angelgvalentin7@gmail.com>
2022-07-07 10:26:00 -0500

    Changed search button color to red

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,5 +1,13 @@
 import React from "react";
-import {Navbar, Container, Button, Nav, NavDropdown, Form, FormControl} from "react-bootstrap";
+import {
+    Navbar,
+    Container,
+    Button,
+    Nav,
+    NavDropdown,
+    Form,
+    FormControl,
+} from "react-bootstrap";
 
 const Navbar2 = () => {
     return (
@@ -9,20 +17,37 @@ const Navbar2 = () => {
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="me-auto my-2 my-lg-0" navbarScroll>
                         <Nav.Link href="/">
-                            <i style={{color: "white"}} class="fa-solid fa-house-chimney"></i>
+                            <i
+                                style={{color: "white"}}
+                                class="fa-solid fa-house-chimney"
+                            ></i>
                         </Nav.Link>
-                        <NavDropdown title="Categories" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="/guitars">Guitars</NavDropdown.Item>
-                            <NavDropdown.Item href="/amps">Amps</NavDropdown.Item>
-                            <NavDropdown.Item href="/pedals">Pedals</NavDropdown.Item>
+                        <NavDropdown
+                            title="Categories"
+                            id="navbarScrollingDropdown"
+                        >
+                            <NavDropdown.Item href="/guitars">
+                                Guitars
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/amps">
+                                Amps
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/pedals">
+                                Pedals
+                            </NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link href="your-listings">Your Listings</Nav.Link>
 
                         <Nav.Link href="/cart">Cart</Nav.Link>
                     </Nav>
                     <Form className="d-flex">
-                        <FormControl type="search" placeholder="Search" className="me-2" aria-label="Search" />
-                        <Button variant="outline-success">Search</Button>
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-danger">Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Container>
